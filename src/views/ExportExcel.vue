2775c5999ecaf4acfbc536a78cd7bb5084a3a8a3
18866805256 <3407085928@qq.com>
2023-07-22 10:40:21 +0800

    完成表格导入导出功能

--- src/views/ExportExcel.vue
+++ src/views/ExportExcel.vue
@@ -1,5 +1,38 @@
 <template>
-  <h1>e</h1>
+  <div>
+    <el-table :data="tableData" v-if="tableData.length > 0">
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="gender" label="性别"></el-table-column>
+    </el-table>
+
+    <el-button type="primary" @click="exportToExcel">导出Excel</el-button>
+  </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { ElTable, ElTableColumn, ElButton } from 'element-plus'
+import *as XLSX from 'xlsx'
+
+interface Data {
+  name: string
+  age: number
+  gender: string
+}
+
+const tableData = reactive<Data[]>([
+  { name: '张三', age: 20, gender: '男' },
+  { name: '李四', age: 25, gender: '女' },
+  { name: '王五', age: 30, gender: '男' },
+])
+
+const exportToExcel = () => {
+  const worksheet = XLSX.utils.json_to_sheet(tableData)
+  const workbook = XLSX.utils.book_new()
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
+  XLSX.writeFile(workbook, 'data.xlsx')
+}
+</script>
+
+<style scoped></style>
--- src/views/ImportExcel.vue
+++ src/views/ImportExcel.vue
@@ -1,5 +1,72 @@
 <template>
-  <h1>d</h1>
+  <div>
+    <el-upload class="upload-excel" :action="''" :before-upload="handleBeforeUpload" :on-success="handleSuccess"
+      :on-error="handleError" :accept="'.xlsx'">
+      <el-button size="small" type="primary">选择文件</el-button>
+    </el-upload>
+
+    <el-table :data="tableData" v-if="tableData.length > 0">
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="gender" label="性别"></el-table-column>
+    </el-table>
+  </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { ElUpload, ElButton, ElTable, ElTableColumn, ElMessageBox } from 'element-plus'
+import *as XLSX from 'xlsx'
+
+interface Data {
+  name: string
+  age: number
+  gender: string
+}
+
+const tableData = reactive<Data[]>([])
+
+const handleBeforeUpload = (file: File) => {
+  const reader = new FileReader()
+  reader.onload = (e: ProgressEvent<FileReader>) => {
+    const data = new Uint8Array(e.target?.result as ArrayBuffer)
+    const workbook = XLSX.read(data, { type: 'array' })
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+
+    if (jsonData.length > 0) {
+      const headers = jsonData[0]
+      const rows = jsonData.slice(1)
+
+      tableData.length = 0
+      rows.forEach((row: any) => {
+        const rowData: Data = {
+          name: row[0] || '',
+          age: row[1] || 0,
+          gender: row[2] || '',
+        }
+        tableData.push(rowData);
+      })
+    } else {
+      ElMessageBox.alert('Excel中没有有效数据', '提示')
+    }
+  }
+
+  reader.readAsArrayBuffer(file)
+  return false // 取消自动上传
+}
+
+const handleSuccess = () => {
+  // 处理上传成功逻辑
+}
+
+const handleError = () => {
+  // 处理上传失败逻辑
+}
+</script>
+
+<style scoped lang="scss">
+.upload-excel {
+  margin-bottom: 20px;
+}
+</style>
